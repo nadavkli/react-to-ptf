4a1966c9f012c27a819e8fecfd05085028c0673c
nadavkli <nadavk@realfriend.ai>
2024-03-17 11:53:56 +0200

    feat: return buffer

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 
 export type ConvertToPdfOptions = {
-    outputPath: string;
+    outputPath?: string;
     puppeteerOptions?: puppeteer.LaunchOptions;
 };
 
-export async function convertToPdf(component: React.ReactElement, options: ConvertToPdfOptions): Promise<void> {
+export async function convertToPdf(component: React.ReactElement, options: ConvertToPdfOptions): Promise<Buffer> {
     const browser = await puppeteer.launch(options.puppeteerOptions);
     try {
         const page = await browser.newPage();
         const htmlContent = ReactDOMServer.renderToStaticMarkup(component);
         await page.setContent(htmlContent);
-        await page.pdf({path: options.outputPath, format: 'A4'});
+        return await page.pdf({path: options?.outputPath, format: 'A4'});
     } catch (error) {
         console.error('Error generating PDF:', error);
         throw error;
